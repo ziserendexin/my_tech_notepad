cbbaa1a1e169c2a7ea48ade7c86f3624b0a06db3
ziserendexin <32018376+ziserendexin@users.noreply.github.com>
2019-11-19 14:15:57 +0800

    add

---
+++ 12306/Readme.md
@@ -0,0 +1,12 @@
+感觉主要研究
+
+ [机构为 中国铁道科学研究院电子计算技术研究所 的文献](http://gb.oversea.cnki.net/kcms/detail/search.aspx?dbcode=CJFD&sfield=inst&skey=%e4%b8%ad%e5%9b%bd%e9%93%81%e9%81%93%e7%a7%91%e5%ad%a6%e7%a0%94%e7%a9%b6%e9%99%a2%e7%94%b5%e5%ad%90%e8%ae%a1%e7%ae%97%e6%8a%80%e6%9c%af%e7%a0%94%e7%a9%b6%e6%89%80&code=0163385;)
+
+就能搞清楚很多了。
+
+
+
+铁路大数据平台架构研究
+Research and design of railway big data platform architecture
+
+http://new.gb.oversea.cnki.net/kcms/detail/detail.aspx?filename=TLJS201908001&dbcode=CJFQ&dbname=CJFDTEMP&v=
--- Let`s go/test0/main.go
+++ Let`s go/test0/main.go
@@ -1,40 +1,61 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.package main
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"os/exec"
 
-// Send the sequence 2, 3, 4, ... to channel 'ch'.
-func generate(ch chan int) {
-	for i := 2; ; i++ {
-		ch <- i // Send 'i' to channel 'ch'.
-	}
-}
+	"github.com/kennygrant/sanitize"
+)
 
-// Copy the values from channel 'in' to channel 'out',
-// removing those divisible by 'prime'.
-func filter(in, out chan int, prime int) {
-	for {
-		i := <-in // Receive value of new variable 'i' from 'in'.
-		if i%prime != 0 {
-			out <- i // Send 'i' to channel 'out'.
-		}
-		if i%100 == 0 && i > 7027 {
-			fmt.Print("(", prime, ",", i, ")")
-		}
-	}
-}
-
-// The prime sieve: Daisy-chain filter processes together.
 func main() {
-	in := make(chan int) // Create a new channel.
-	go generate(in)      // Start generate() as a goroutine.
-	for {
-		prime := <-in
-		fmt.Print(prime, " ")
-		out := make(chan int)
-		go filter(in, out, prime)
-		in = out
+	uri := "rtsp://10.25.144.106:554/live?camera=146&user=duansishengtims&pass=E0QjCAJLEHFC"
+	URL, _ := url.Parse(uri)
+	// fmt.Sprintf(URL)
+	fmt.Println(URL.Hostname())
+	fmt.Println(URL.Port())
+	fmt.Println(sanitize.Path(URL.Path))
+	fmt.Println(sanitize.Path(URL.RawQuery))
+	path := sanitize.BaseName(fmt.Sprintf("%s-%s-%s-%s", URL.Hostname(), URL.Port(), sanitize.Path(URL.Path), sanitize.Path(URL.RawQuery)))
+	fmt.Println(path)
+
+	cmd := exec.Command(
+		"ffmpeg",
+		"-y",
+		"-fflags",
+		"nobuffer",
+		"-rtsp_transport",
+		"tcp",
+		"-i",
+		fmt.Sprintf("\"%s\"", uri),
+		"-vsync",
+		"0",
+		"-copyts",
+		"-vcodec",
+		"copy",
+		"-movflags",
+		"frag_keyframe+empty_moov",
+		"-an",
+		"-hls_flags",
+		"delete_segments+append_list",
+		"-f",
+		"hls",
+		"-segment_list_flags",
+		"live",
+		"-hls_time",
+		"1",
+		"-hls_list_size",
+		"3",
+		"-hls_segment_filename",
+		fmt.Sprintf("%s/%%d.ts", path),
+		fmt.Sprintf("%s/index.m3u8", path),
+	)
+	fmt.Println(cmd.)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
 	}
 }
